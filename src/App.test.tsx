2a343e37dcc19b6801f3967652164da163534564
Priest Ombugadu <sabopriest@gmail.com>
2023-10-31 17:38:39 +0100

    chore: added cypress e2e test, and components unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,9 @@
-import React from "react"
-import { screen } from "@testing-library/react"
-import { render } from "./test-utils"
-import { App } from "./App"
+import { render } from "./test-utils";
+import { App } from "./App";
 
-test("renders learn react link", () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
-})
+test("This is a demo test", () => {
+  render(<App />);
+  // const linkElement = screen.getByText(/learn chakra/i);
+  // expect(linkElement).toBeInTheDocument();
+  expect(1).toStrictEqual(1);
+});
